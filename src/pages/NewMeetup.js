49c56c5561f678d61238531b62a2d68195394fd3
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-05-23 21:29:16 -0300

    -

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,28 @@
 import React from "react";
 import NewMeetupsForm from "../meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 
 export default function NewMeetups() {
+const history = useHistory();
+
+  function AddMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-96008-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupsForm />
+      <NewMeetupsForm onAddMeetup={AddMeetupHandler} />
     </section>
-  )
+  );
 }

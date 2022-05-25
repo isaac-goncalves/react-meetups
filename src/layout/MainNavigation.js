309296ebe8a07afcf22a3623906f18d3336cd703
Isaac Correia <isaac.correia.2406@gmail.com>
2022-05-25 09:50:17 -0300

    -

--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import classes from './MainNavigation.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../store/favorites-context";
+import {useContext} from 'react'
 
 export default function MainNavigation() {
+const  FavoritesCtx = useContext(FavoritesContext)
+
     return (
-        <header className={classes.header}>
-            <div className={classes.logo}>React Meetups</div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>All Meetups</Link>
-                    </li>
-                    <li>
-                        <Link to='/new-meetup'>Add New Meetup</Link>
-                    </li>
-                    <li>
-                        <Link to='/favorites'>
-                            My Favorites
-                            <span className={classes.badge}></span>
-                            </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>{FavoritesCtx.totalFavorites}</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import MeetupList from "../meetups/MeetupList";
+import FavoritesContext from "../store/favorites-context";
 
 export default function Favorites() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+let content;
+
+if(favoritesCtx.totalFavorites ===  0)
+{
+  content = <p> You got no favorites yet. Start adding some? </p>
+}else
+{
+  content = <MeetupList meetups={favoritesCtx.favorites}/>
+}
   return (
-    <div>Favorites</div>
-  )
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
